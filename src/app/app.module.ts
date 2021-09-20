7f5a0f602ec5e2edfba11132ef9f4ad8451d0fbd
Bercy <bercy.apaza@gmail.com>
2021-09-20 10:57:30 -0500

    Se agrega registro de medicamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddMedicamentoComponent } from './components/add-medicamento/add-medicamento.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMedicamentoComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
